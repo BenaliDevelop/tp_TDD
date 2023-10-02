0b42b920beb201b32edec5e51ba1c6496d3d1082
BenaliDevelop <mybenali30@gmail.com>
2023-10-02 12:40:30 +0200

    1er test fizz_buzz

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -5,3 +5,6 @@ class Test_FizzBuzz (unittest.TestCase):
 
     def setUp(self):
         self.instance=fizzbuzz()
+
+    def test_affiche_sans_param(self):
+        self.assertEqual(self.instance.affiche(), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz.....9798FizzBuzz")
